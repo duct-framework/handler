a7d1310dbe5d93be02fadf8ce81f782c7aa70246
James Reeves <jreeves@weavejester.com>
2024-12-19 09:28:57 +0000

    Add more file handler tests

--- test/duct/handler/file_test.clj
+++ test/duct/handler/file_test.clj
@@ -30,3 +30,26 @@
                (update :headers dissoc "Last-Modified"))))
     (is (= {:status 404, :body "Not Found"}
            (handler {:request-method :get, :uri "/static/bar.txt"})))))
+
+(deftest test-security
+  (let [root    "test/duct/handler/files"
+        handler (ig/init-key :duct.handler/file
+                             {:paths {"/static/" {:root root}}})]
+    (is (nil? (handler {:request-method :get, :uri "/../file_test.clj"})))))
+
+(deftest test-async-handler
+  (let [root    "test/duct/handler/files"
+        handler (ig/init-key :duct.handler/file
+                             {:paths {"/static/" {:root root}}})
+        respond (promise)
+        raise   (promise)]
+    (handler {:request-method :get, :uri "/static/foo.txt"}
+             respond raise)
+    (is (realized? respond))
+    (is (not (realized? raise)))
+    (is (= {:status 200
+            :headers {"Content-Type" "text/plain"
+                      "Content-Length" "4"}
+            :body    (io/file root "foo.txt")}
+           (-> (deref respond 100 nil)
+               (update :headers dissoc "Last-Modified"))))))
