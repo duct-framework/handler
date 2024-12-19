e20c59c94944ab3f1fa7d3afa74172404a2179c9
James Reeves <jreeves@weavejester.com>
2024-12-19 08:50:06 +0000

    Add static file handler

--- src/duct/handler/file.clj
+++ src/duct/handler/file.clj
@@ -1,1 +1,28 @@
-(ns duct.handler.file)
+(ns duct.handler.file
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]
+            [duct.handler.static :as static]
+            [integrant.core :as ig]
+            [ring.util.async :as async]))
+
+(defn- match-path [uri paths]
+  (first (filter #(str/starts-with? uri (key %)) paths)))
+
+(defn- path-handler [paths make-response not-found-response]
+  (let [paths (sort-by (comp count key) paths)]
+    (fn handler
+      ([{:keys [uri]}]
+       (if-some [[path opts] (match-path uri paths)]
+         (or (make-response (subs uri (count path)) opts)
+             not-found-response)
+         not-found-response))
+      ([request respond raise]
+       (async/raising raise (respond (handler request)))))))
+
+(defmethod ig/init-key :duct.handler/file [_ {:keys [paths not-found]}]
+  (path-handler paths
+                (fn [path {:keys [root]}]
+                  (let [body (io/file root path)]
+                    (when (.exists body)
+                      (static/static-response {:status 200, :body body}))))
+                not-found))
--- test/duct/handler/file_test.clj
+++ test/duct/handler/file_test.clj
@@ -1,1 +1,32 @@
-(ns duct.handler.file-test)
+(ns duct.handler.file-test
+  (:require [clojure.java.io :as io]
+            [clojure.test :refer [deftest is]]
+            [duct.handler.file :as file]
+            [integrant.core :as ig]))
+
+(deftest test-file-handler
+  (let [root    "test/duct/handler/files"
+        handler (ig/init-key :duct.handler/file
+                             {:paths {"/static/" {:root root}}})]
+    (is (= {:status 200
+            :headers {"Content-Type" "text/plain"
+                      "Content-Length" "4"}
+            :body    (io/file root "foo.txt")}
+           (-> (handler {:request-method :get, :uri "/static/foo.txt"})
+               (update :headers dissoc "Last-Modified"))))
+    (is (nil? (handler {:request-method :get, :uri "/static/bar.txt"})))
+    (is (nil? (handler {:request-method :get, :uri "/foo.txt"})))))
+
+(deftest test-not-found
+  (let [root    "test/duct/handler/files"
+        handler (ig/init-key :duct.handler/file
+                             {:paths {"/static/" {:root root}}
+                              :not-found {:status 404, :body "Not Found"}})]
+    (is (= {:status 200
+            :headers {"Content-Type" "text/plain"
+                      "Content-Length" "4"}
+            :body    (io/file root "foo.txt")}
+           (-> (handler {:request-method :get, :uri "/static/foo.txt"})
+               (update :headers dissoc "Last-Modified"))))
+    (is (= {:status 404, :body "Not Found"}
+           (handler {:request-method :get, :uri "/static/bar.txt"})))))
