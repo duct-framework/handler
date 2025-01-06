3020521456a35ac5d8854ec36d3766167878cf5e
James Reeves <jreeves@weavejester.com>
2025-01-06 16:19:14 +0000

    Add description and URL to project file

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject org.duct-framework/handler "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Common Ring handlers for the Duct framework"
+  :url "https://github.com/duct-framework/handler"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.12.0"]
