3c441ad2c6eb90a546fab9f7c31abf63f6dc0080
James Reeves <jreeves@weavejester.com>
2025-09-13 01:40:41 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/duct-framework/handler"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.12.0"]
-                 [integrant "0.13.1"]
-                 [ring/ring-core "1.13.0"]])
+  :dependencies [[org.clojure/clojure "1.12.2"]
+                 [integrant "1.0.0"]
+                 [ring/ring-core "1.15.1"]])
